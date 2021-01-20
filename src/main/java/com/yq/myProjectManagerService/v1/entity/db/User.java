89af79aa09f116548ff26405879b10bf0acff8c9
Ye <robertyea@163.com>
2021-01-20 22:08:17 +0800

    lombok配置

--- src/main/java/com/yq/myProjectManagerService/v1/entity/db/User.java
+++ src/main/java/com/yq/myProjectManagerService/v1/entity/db/User.java
@@ -12,9 +12,8 @@ import java.io.Serializable;
  * @version 1.0
  * @date 2021/01/16
  */
-public class User implements Serializable {
-
-    private static final long serialVersionUID = 1L;
+@Data
+public class User {
 
     private int id;
 
@@ -25,55 +24,4 @@ public class User implements Serializable {
 
     private String password;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUuid() {
-        return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public User() {
-    }
-
-    public User(int id, String uuid, String userName, String password) {
-        this.id = id;
-        this.uuid = uuid;
-        this.userName = userName;
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", uuid='" + uuid + '\'' +
-                ", userName='" + userName + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
